dc144c179677e2a51c6683369774122c16eb637f
Elvira Galieva <elvira.6.galieva@yandex.ru>
2023-12-31 21:31:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function playSound1() {
+  var audio = document.getElementById('sound-1')
+  audio.play()
+}
+function playSound2() {
+  var audio = document.getElementById('sound-2')
+  audio.play()
+}
+function playSound3() {
+  var audio = document.getElementById('sound-3')
+  audio.play()
+}
+function playSound4() {
+  var audio = document.getElementById('sound-4')
+  audio.play()
+}
